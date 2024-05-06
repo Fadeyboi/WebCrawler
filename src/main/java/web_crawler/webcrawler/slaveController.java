b2e4018821d489693f450a8778f26c9912cbc270
AbdallahAlghamdi <75899918+AbdallahAlghamdi@users.noreply.github.com>
2024-05-06 20:46:24 +0300

    Adding text, textfield and button in the slave screen. Also, adding textformating for the port number.

--- src/main/java/web_crawler/webcrawler/slaveController.java
+++ src/main/java/web_crawler/webcrawler/slaveController.java
@@ -1,4 +1,44 @@
 package web_crawler.webcrawler;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.function.UnaryOperator;
+
 public class slaveController {
+    @FXML
+    TextField portInput;
+
+    @FXML
+    Text ipAddressText;
+    @FXML
+    private void runSlave(){
+
+    }
+    @FXML
+    private void initialize() throws IOException {
+        System.out.println("TEST");
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
+        ipAddressText.setText("IP address of Slave is: " + InetAddress.getLocalHost().getHostAddress());
+        TextFormatter<String> portFormatter = new TextFormatter<>(filter);
+        portInput.setTextFormatter(portFormatter);
+    }
+    UnaryOperator<TextFormatter.Change> filter = change -> {
+        String newCharacter = change.getText();
+        String fullText = change.getControlNewText();
+        if(newCharacter.matches("[0-9]*"))
+            if(fullText.length()<7)
+               if(!fullText.isEmpty())
+                   if(Integer.parseInt(fullText)<65536)
+                       return change;
+
+        return null;
+    };
+
 }

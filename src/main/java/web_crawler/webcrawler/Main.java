a59cecaca312a29801f1c660e1cc84985051bd6d
Fadeyboi <fadealone44@gmail.com>
2024-05-08 21:21:43 +0300

    Test Commit

--- src/main/java/web_crawler/webcrawler/Main.java
+++ src/main/java/web_crawler/webcrawler/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("userType.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 150);
-        stage.setTitle("WebCrawler!");
+        stage.setTitle("WebCrawler");
         stage.setScene(scene);
         stage.show();
     }
